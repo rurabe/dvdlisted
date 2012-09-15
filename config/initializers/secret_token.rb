6018e547084cd672dd95cb653274f8175bbc1df7
rurabe <rurabe@gmail.com>
2012-09-15 12:20:59 -1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvdlisted::Application.config.secret_token = '4af9436a17df638fffc65dffdae89ae69a318367508763f16bf8a63c016d624bb2f1a37a496c2886bcd8e79fde1ac01e313ee6ac1cd401b267babb81cd72da06'
