94a432c5e62ab572867ae8327462375e85846573
rurabe <rurabe@gmail.com>
2012-09-17 22:30:56 -1000

    fixed bug that let you do duplicates from the slelect boxes

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,6 +7,6 @@ class Role < ActiveRecord::Base
   validates :role_type, :inclusion => { :in => ['actor','director'] }
   validates_presence_of :dvd_id, :person_id
   validates_uniqueness_of :role_type, :scope => :dvd_id, :if => Proc.new { |role| role.role_type == 'director'}
-
+  validates_uniqueness_of :person_id, :scope => [:dvd_id,:role_type]
 
 end
